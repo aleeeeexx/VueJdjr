d82c73df1bcab5caddd1c4d79ad5f0f831af1ad0
alex <yalibuda@163.com>
2019-10-06 14:02:55 +0800

    修改了webpack配置,可以压缩js和css文件

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-// const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin=require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 
 module.exports = env => {
@@ -27,12 +28,24 @@ module.exports = env => {
           NODE_ENV: '"production"'
         }
       }),
-      // new ExtractTextPlugin("style.css", {
-      //   ignoreOrder: true
-      // }),
       new MiniCssExtractPlugin({
         filename: "style.css"
+      }),
+      new UglifyJsPlugin(),
+      new OptimizeCSSAssetsPlugin({
+        assetNameRegExp: /\.css$/g,
+        cssProcessor: require('cssnano'),
+        cssProcessorPluginOptions:{
+          preset:['default', {
+				        discardComments: {
+				            removeAll: true,
+				        },
+				        normalizeUnicode: false
+				    }]
+        },
+        canPrint: true
       })
+
     )
   }
   return {
